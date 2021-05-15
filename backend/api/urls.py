da7ba7ee46c0ab8f737b5c5aa9bdc055c1d0080b
chenrui <chenruisero@outlook.com>
2021-05-16 00:16:11 +0800

    tweet store to couchDB

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', ),
 ]
--- harvester/Twitter_API/single_search.py
+++ harvester/Twitter_API/single_search.py
@@ -1,5 +1,6 @@
 import tweepy
 import json
+from TweetStore import TweetStore
 #import couchdb
 #import time
 
@@ -11,6 +12,7 @@ consumer_secret = "aDaoN5IWq8NiLZ8GsqK8dzAkBy683yjJt4RcZZ2sVf67hH6f6F"
 access_token = "993804554880794624-K0gOvq6FYnycALj6I31cPeqjxxeL8VC"
 access_token_secret = "TC0xFxOzRGAJb6R9q6PaQOiDDUG9Csq14HBtyDGNdMlSw"
 
+COUCH_DATABASE = 'test_db'
 
 #Handel Twitter Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -19,14 +21,18 @@ auth.set_access_token(access_token, access_token_secret)
 # Construct the API instance
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-
+storage = TweetStore(COUCH_DATABASE)
 result_dic = {}
+
 def search_location(query, geocode, max_count):
     try:
         search_tweets = api.search(q=query, geocode=geocode, count=max_count)
 
         for tweet in search_tweets:
             result_dic[tweet.id] = tweet._json
+            #storage.save_tweet(tweet)
+            print(type(tweet._json))
+            storage.save_tweet(tweet._json)
 
         json_object = json.dumps(result_dic, indent=4)
         with open("output.json", "w") as outfile:
