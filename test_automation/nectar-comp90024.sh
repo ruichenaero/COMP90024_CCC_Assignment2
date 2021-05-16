1f1230b706b45d98a7db8e62d9ceb7c5244dc2c8
whuo0002 <whuo0002@student.monash.edu>
2021-05-17 00:31:59 +0800

    ansible deploy ver 0.1 testing

---
+++ test_automation/nectar-comp90024.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+. ./unimelb-comp90024-2021-grp-47-openrc.sh
+
+# Configure basic instances/nodes
+ansible-playbook --ask-become-pass config_nectar.yaml 
+
+# Deploy all instances/nodes environment
+ansible-playbook --ask-become-pass deploy_nectar.yaml -i inventory/hosts.ini
+
+# Lanuch nector application
+ansible-playbook --ask-become-pass launch_nectar.yaml -i inventory/hosts.ini
