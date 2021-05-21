dfdbb03c61de0ba0b24614263a23106942d4d777
ruoyu.cr <ruoyu.cr@alibaba-inc.com>
2021-05-21 13:02:17 +0800

    fix

--- harvester/Twitter_API/getTweet.py
+++ harvester/Twitter_API/getTweet.py
@@ -17,7 +17,7 @@ storage_region.create_view(index, geo_mac, reduce_func=geo_reduce)
 
 print('tweet count of region_tweet db is %d\n' % storage_region.count_tweets())
 
-print('tweet count with coordinate is %d\n' % storage_region.twitter_with_geo())
+#print('tweet count with coordinate is %d\n' % storage_region.twitter_with_geo())
 
 print("The tweet count of each region", storage_region.get_region_count())
 
