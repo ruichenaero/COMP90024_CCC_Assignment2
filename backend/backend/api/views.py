af21f2c9885a2b3f670f4f576ede63481cc410c3
ruoyu.cr <ruoyu.cr@alibaba-inc.com>
2021-05-25 00:57:06 +0800

    fix: easy backend

--- backend/backend/api/views.py
+++ backend/backend/api/views.py
@@ -2,13 +2,15 @@ import json
 
 from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseBadRequest, JsonResponse
 
-from backend.backend.api.utils import *
-from backend.backend.couchDAO.couchDBHandler import *
+#from backend.backend.api.utils import *
+#from backend.backend.couchDAO.couchDBHandler import *
 
+"""
 couch_db_banlancer = CouchDBBalancer()
 couch_db_banlancer.connect_database(COUCHDB_REGION_TWEET_DB)
 tweet_database = couch_db_banlancer.get_current_database()
 tweet_parser = Parse("../common/AFINN.txt")
+"""
 
 def region_tweet_count(request):
     response = JsonResponse({"name":"tweet_count"})
@@ -28,12 +30,14 @@ def region_topic_count_sport(request):
     response['Access-Contro1-Allow-origin'] = "*"  # 设置请求头
     return response
 
+"""
 def sentiment_scatter(request):
     tweet_database = couch_db_banlancer.get_current_database()
     coordinates_map = {}
     for doc in tweet_database.view('statistic/coordinates_map'):
        coordinates_map[str(doc.key)] = doc.value
     return JsonResponse(coordinates_map)
+
 
 def region_sport_count(docs):
     region_sport_count = {}
@@ -66,6 +70,7 @@ def region_sport_count(docs):
 
 def region_sentimentscore_count(docs):
     region_sentimentscore_count = {}
+    region_sentimentscore_geo = {}
     for value in REGION_MAP.values():
         region_sentimentscore_count[value] = 0
 
@@ -77,11 +82,18 @@ def region_sentimentscore_count(docs):
     with open("region_sentimentscore_count", "a") as outfile:
         outfile.write(json_object)
 
+    region_sentimentscore_geo = {}
+
+def make_result(func = HttpResponse, respose=None):
+    result = func(respose)
+    result
+
+
 if __name__ == '__main__':
 
     docs = tweet_database.view('statistic/region_tweet_map')
     region_sentimentscore_count(docs)
-
+"""
 
 
 
