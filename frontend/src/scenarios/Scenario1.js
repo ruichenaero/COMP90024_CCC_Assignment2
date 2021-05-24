a34e2e40baebeac02995f0cf3edf4b13eec1356b
whuo0002 <whuo0002@student.monash.edu>
2021-05-25 00:15:02 +0800

    react try 1

--- frontend/src/scenarios/Scenario1.js
+++ frontend/src/scenarios/Scenario1.js
@@ -24,7 +24,7 @@ export default function Scenario1() {
 
   useEffect(() => {
     
-    axios.get(`http://127.0.0.1:80/api/region_topic_count/food/`)
+    axios.get(`http://localhost:80/api/region_topic_count/food/`)
       .then(res => { 
         setIsLoaded(true); 
         setCountState(res.data.name);
