e98abf41bcde840e614ff45806a86b129c859f1a
jessica-ma3 <jessica@github.no.reply.com>
2021-05-25 03:30:01 +1000

    Update Scenario1.js

--- frontend/src/scenarios/Scenario1.js
+++ frontend/src/scenarios/Scenario1.js
@@ -28,7 +28,7 @@ export default function Scenario1() {
       .then(res => { 
         setIsLoaded(true); 
         setCountState(res.data.name);
-
+        console.log(res.data.name);
       }, (error) => {
           setIsLoaded(true);
           setError(error);
@@ -59,7 +59,7 @@ export default function Scenario1() {
   } else if (!isLoaded) {
     return <div>Loading...</div>;
   } else {
-
+    console.log(count.name);
     return (
       
       <Layout style={{ minHeight: '100vh' }}>
