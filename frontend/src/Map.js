b1e2a033aaf0bfcc24b1cba3deef1d3deec0712e
jessica-ma3 <jessica@github.no.reply.com>
2021-05-26 04:54:36 +1000

    update

--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -3,6 +3,7 @@ import SidePanel from "./components/SidePanel";
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import { Layout } from 'antd';
 import Hospital_Statistics from './data/Hospital_Statistics.json';
+import Regions from './data/regions.json';
 import axios from "axios";
 
 mapboxgl.accessToken = 'pk.eyJ1IjoieWlmZXlhbmcxIiwiYSI6ImNrb251MG44ZzA0Njkyd3BweWFyMWJvcjYifQ.oEO3lpWd3GLwRu13euHIvA';
@@ -18,7 +19,17 @@ export default function Map() {
   //const [isLoaded, setIsLoaded] = useState(false);
   //const [isAdded, setIsAdded] = useState(false);
   const aurinData = Hospital_Statistics.features;
+  const regions = Regions;
+  const counts = Regions.features.map(region => {
+    return parseInt(region.properties.count);    
+    });
+  
+  const math = require("mathjs");
+  const maxCount = math.max(counts);
+  const minCount = math.min(counts);
+  const add = (maxCount-minCount)/5;
 
+  
   /*
   const getData=()=>{
     fetch('/Hospital_Statistics.json'
@@ -40,13 +51,34 @@ export default function Map() {
       });
   }
   */
+   const colors = ['#b3d9ff', '#80bfff','#4da6ff', '#1a8cff', '#0073e6', '#0059b3'];
+   const colorClasses = getCountRange(regions, colors);
+
+
+  function getCountRange(source, colors) {
+    var phvals = source.features.map(f => f.properties.count);
+    var min = Math.min(...phvals);
+    var max = Math.max(...phvals);
+    var range = max-min;
+
+    var iter = 0;
+    var colorClasses = [];
+    var len = colors.length;
+    colors.forEach(element => {
+        iter += 1;
+        colorClasses.push(min+(iter/len)*range)
+        colorClasses.push(element)
+    });
+
+    return (colorClasses);
+}
 
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/yifeyang1/ckoo0ifyg0vrn17o15ro2bicw/draft',
+      style: 'mapbox://styles/yifeyang1/ckp41vh7i0pli19o3x6fe84jh/draft',
       center: [lng, lat],
       zoom: zoom
     });
@@ -85,25 +117,51 @@ export default function Map() {
     console.log(aurinData);
 
     map.current.on('load', () => {
-      map.current.addSource('hospital_location', {
-        type: 'geojson',
-        data: {
-          type: 'FeatureCollection',
-          features: aurinData.map(hospital => {
-            return {
-              type: 'Feature',
-              geometry: {
-                type: 'Point',
-                coordinates: hospital.geometry.coordinates,
-              },
-            };
-          }),
-        },
-      });
+      map.current.loadImage('https://i.loli.net/2021/05/25/rdNiyRkwZWafj5x.png', function (error, image) {
+                    if (error) throw error;
+                    map.current.addImage('exclamation', image); //38x55px, shadow adds 5px
+        
+                });
+        map.current.addSource('hospital_location', {
+          type: 'geojson',
+          data: {
+            type: 'FeatureCollection',
+            features: aurinData.map(hospital => {
+              return {
+                type: 'Feature',
+                geometry: {
+                  type: 'Point',
+                  coordinates: hospital.geometry.coordinates,
+                },
+              };
+            }),
+          },
+        });
+      
+        map.current.addSource('regions', {
+          type: 'geojson',
+          data: {
+            type: 'FeatureCollection',
+            features: regions.features.map(region => {
+              return {
+                type: 'Feature',
+                properties: {
+                  radius: region.properties.radius,
+                },
+                geometry: {
+                  type: 'Point',
+                  coordinates: region.geometry.coordinates,
+                 },
+                };
+              }),
+            },
+          
+        });
+      
 
       map.current.addLayer({
         'id': 'hospitals_loc',
-        'type': 'circle',
+        /*'type': 'circle',
         'source': 'hospital_location',
         'layout': {
           // Make the layer visible by default.
@@ -112,13 +170,46 @@ export default function Map() {
         'paint': {
           'circle-radius': 8,
           'circle-color': 'rgba(55,148,179,1)',
+        },*/
+        'type': 'symbol',
+        'source': 'hospital_location',
+        'layout': {
+          // Make the layer visible by default.
+          'icon-image': 'exclamation',
+          'icon-size': 0.05,
+         
         },
+        
         //'source-layer': 'museum-cusco'
         //rgba(55,148,179,1)
       });
+
+      map.current.addLayer({
+        'id': 'regions_circle',
+        'type': 'circle',
+        
+        
+        'source': {
+            type: 'vector',
+            url: 'mapbox://styles/yifeyang1/ckp41vh7i0pli19o3x6fe84jh/draft'
+        },
+        'source-layer': 'melbourne-region',
+        'paint': {
+            'circle-color': ['interpolate', ['linear'], ['get', 'count'], minCount, '#b3d9ff', math.round(minCount+add), '#80bfff', math.round(minCount+add*2), '#4da6ff', math.round(math.round(minCount+add*3)), '#1a8cff',  math.round(minCount+add*4), '#0073e6', maxCount,  '#0059b3'],
+            'circle-opacity': 0.75,
+            'circle-radius': ['interpolate', ['linear'], ['get', 'count'], minCount, 10, math.round(minCount+add), 15, math.round(minCount+add*2), 20, math.round(minCount+add*3), 25, math.round(minCount+add*4), 30, maxCount, 35]
+        },
     });
+    });
+    //console.log(counts);
+    console.log(maxCount);
+    console.log(minCount);
+    console.log(add);
+    console.log(math.round(minCount+add));
+    //console.log(colorClasses[0]);
 
   }, []);
+
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
