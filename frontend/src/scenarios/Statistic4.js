7539c00c995ef3e4b6eaa849148e03d9c83d5e7c
jessica-ma3 <jessica@github.no.reply.com>
2021-05-27 02:30:23 +1000

    update

---
+++ frontend/src/scenarios/Statistic4.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import SidePanel from "../components/SidePanel";
+import { Layout, Breadcrumb } from 'antd';
+import '../App.css';
+import { Bar, Pie, Line } from 'react-chartjs-2';
+import region_sentiment_count from '../data/region_sentiment_count.json';
+import Income_data from '../data/Income_data.json';
+import { Row, Col } from 'antd';
+import food_time_count from '../data/food_time_count.json';
+import sport_time_count from '../data/sport_time_count.json';
+
+
+const { Header, Content, Footer, Sider } = Layout;
+
+export default function Statistic4() {
+
+
+    const data = {
+        labels: Object.keys(food_time_count),
+        datasets: [
+          {
+            label: 'Count of tweets related to food',
+            data: Object.values(food_time_count),
+            fill: false,
+            backgroundColor: 'rgba(75, 192, 192)',
+            borderColor: 'rgba(75, 192, 192, 0.2)',
+          },
+        ],
+      };
+      
+      const options = {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      };
+    
+      const data_sport = {
+        labels: Object.keys(food_time_count),
+        datasets: [
+          {
+            label: 'Count of tweets related to sport',
+            data: Object.values(food_time_count),
+            fill: false,
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgba(255, 99, 132, 0.2)',
+          },
+        ],
+      };
+
+      
+    return (
+        <Layout style={{ minHeight: '100vh' }}>
+          <SidePanel/>
+          <Layout className="site-layout">
+            <Header className="site-layout-background" style={{ padding: 0 }} />
+            <Content style={{ margin: '0 16px' }}>
+              <Breadcrumb style={{ margin: '16px 0' }}>
+              </Breadcrumb>
+              <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                <Line data={data} options={options} />
+                <div style={{ height: 100 }}></div>
+                <Line data={data_sport} options={options} />
+                <div style={{ height: 100 }}></div>
+              </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+          </Layout>
+        </Layout>
+      );
+
+}
